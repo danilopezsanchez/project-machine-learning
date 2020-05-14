4c64b6e5623484ee2eec21068e3d601123d6bff4
Dani <danilopez.pc@gmail.com>
2020-05-14 17:32:57 +0200

    Final code of the project. Deleting the folders of https://github.com/danilopezsanchez/DevOps_Microservices.git

---
+++ run_kubernetes.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=overrider/project
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run project-pod-kube\
+    --image=$dockerpath\
+    --port=80 --labels app=project-pod-kube
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward project-pod-kube 8000:80
+
